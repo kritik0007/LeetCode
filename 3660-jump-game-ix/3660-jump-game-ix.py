657c3d51635d17d8d73bf610c4988f91eb6f8b86
kritik0007 <kritiks702@gmail.com>
2026-05-07 10:50:20 +0530

    Time: 273 ms (36.21%), Space: 81.5 MB (5.17%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import connected_components
+
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n == 1:
+            return nums[:]
+
+        prefix_max = [0] * n
+        prefix_max[0] = nums[0]
+        for i in range(1, n):
+            prefix_max[i] = max(prefix_max[i - 1], nums[i])
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(suffix_min[i + 1], nums[i])
+
+
+        ans = [0] * n
+        start = 0
+        for i in range(n - 1):
+            if prefix_max[i] <= suffix_min[i + 1]:  
+                for k in range(start, i + 1):
+                    ans[k] = prefix_max[i]            
+                start = i + 1
+        for k in range(start, n):                    
+            ans[k] = prefix_max[n - 1]
+
+        return ans
