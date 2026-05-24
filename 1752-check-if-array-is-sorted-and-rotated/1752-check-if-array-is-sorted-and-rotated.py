d6b1a6f8990012dafce55d16d9bfb3a7f3081a8a
kritik0007 <kritiks702@gmail.com>
2026-05-24 22:02:15 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.49%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,3 +1,10 @@
 class Solution:
-    def check(self, nums: List[int]) -> bool:
-        return sum(nums[i - 1] > nums[i] for i in range(len(nums))) < 2
+    def check(self, nums: list[int]) -> bool:
+        breaks = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                breaks += 1
+
+        return breaks <= 1
