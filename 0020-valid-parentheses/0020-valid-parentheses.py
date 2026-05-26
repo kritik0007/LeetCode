c7cc5497c00d296dde74ab391d192ae524160900
kritik0007 <kritiks702@gmail.com>
2026-05-26 17:04:33 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,10 +6,10 @@ class Solution:
             if char in matches:
                 if not stack:
                     return False
-                if stack[-1]==matches[char]:
+                if stack[-1] == matches[char]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(char)    
+                stack.append(char)
         return len(stack) == 0
