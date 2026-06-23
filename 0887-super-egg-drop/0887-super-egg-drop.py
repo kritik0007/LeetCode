32d9df740a982608506239fcb8e074e7bbeab477
kritik0007 <kritiks702@gmail.com>
2026-06-23 09:09:15 +0530

    Time: 0 ms (100%), Space: 19.2 MB (80.47%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.py
+++ 0887-super-egg-drop/0887-super-egg-drop.py
@@ -1,28 +1,11 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        memo = {}
-        
-        def solve(eggs, floors):
-            if floors <= 1 or eggs == 1:
-                return floors
-            if (eggs, floors) in memo:
-                return memo[(eggs, floors)]
-            
-            low, high = 1, floors
-            ans = floors
-            
-            while low <= high:
-                mid = (low + high) // 2
-                break_case = solve(eggs - 1, mid - 1)
-                not_break_case = solve(eggs, floors - mid)
-                
-                ans = min(ans, 1 + max(break_case, not_break_case))
-                if break_case < not_break_case:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            
-            memo[(eggs, floors)] = ans
-            return ans
-            
-        return solve(k, n)
+        dp = [0] * (k + 1)
+        moves = 0
+
+        while dp[k] < n:
+            moves += 1
+            for eggs in range(k, 0, -1):
+                dp[eggs] = dp[eggs] + dp[eggs - 1] + 1
+
+        return moves
