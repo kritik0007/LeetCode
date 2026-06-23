614cd8b94235b2cf4df23b1d5ac8607481db5807
kritik0007 <kritiks702@gmail.com>
2026-06-23 10:15:57 +0530

    Time: 47 ms (41.67%), Space: 28.1 MB (28.65%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.py
+++ 0887-super-egg-drop/0887-super-egg-drop.py
@@ -1,11 +1,14 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        dp = [0] * (k + 1)
-        moves = 0
+        
+    # dp[m][k] = max floors we can check
+    # with m moves and k eggs
+        dp = [[0] * (k+1) for _ in range(n+1)]
+        m = 0  # moves counter
 
-        while dp[k] < n:
-            moves += 1
-            for eggs in range(k, 0, -1):
-                dp[eggs] = dp[eggs] + dp[eggs - 1] + 1
+        while dp[m][k] < n:
+         m += 1
+         for j in range(1, k+1):
+            dp[m][j] = dp[m-1][j-1] + dp[m-1][j] + 1
 
-        return moves
+        return m
